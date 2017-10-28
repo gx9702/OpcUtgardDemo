d278ad0eb8d20f5ac2bd267d2e1240675da3ff5f
zihaozhu <hffhah@126.com>
2017-10-28 13:48:35 +0800

    add some files

--- src/org/zihao/opc/main/Test.java
+++ src/org/zihao/opc/main/Test.java
@@ -6,34 +6,49 @@ import java.util.concurrent.Executors;
 
 import org.jinterop.dcom.common.JIException;
 import org.jinterop.dcom.core.JIVariant;
+import org.openscada.opc.dcom.common.KeyedResultSet;
+import org.openscada.opc.dcom.common.ResultSet;
+import org.openscada.opc.dcom.da.IOPCDataCallback;
+import org.openscada.opc.dcom.da.ValueData;
 import org.openscada.opc.lib.common.ConnectionInformation;
 import org.openscada.opc.lib.da.Group;
 import org.openscada.opc.lib.da.Item;
 import org.openscada.opc.lib.da.Server;
+import org.openscada.opc.lib.da.ServerConnectionStateListener;
+import org.zihao.opc.utils.PropertyFileManager;
 
 public class Test {
 
 	public static void main(String[] args) throws Exception {
-		// create connection information
+		PropertyFileManager.loadPropertyFile("opcclsid.properties");
+		String clsid = PropertyFileManager.getValue("CIMPLICITY");
+
+		System.out.println("clsid=" + clsid);
+
+		if (null == clsid) {
+			return;
+		}
+
 		final ConnectionInformation ci = new ConnectionInformation();
-		ci.setHost("127.0.0.1");
-		ci.setDomain("");
+		ci.setHost("127.1");
+		ci.setDomain("localhost");
+		ci.setClsid(clsid);
 
-		//ci.setProgId(OpcType.CIMPLICITY); // s7 注意 使用progId必须要求dcom配置正确
-
-		// 我们建议使用Clsid，因为使用Grogid时，Openscada的内部处理还是会通过JISystem.getClsidFromProgId(
-		// progId )方法将其转换为Clsid，并且还需要进行服务器上用户的权限的高级配置才可以使用。
-		// ci.setClsid("B3AF0BF6-4C0C-4804-A122-6F3B160F4397");//kepware
-		ci.setClsid("B01241E8-921B-11d2-B43F-204C4F4F5020");// cim
-
-		// DCOM user
+		/** DCOM user */
 		ci.setUser("OpcUser");
 		ci.setPassword("Pass1234");
 
-		// create a new server
 		final Server server = new Server(ci, Executors.newSingleThreadScheduledExecutor());
 		try {
 			server.connect();
+
+			server.addStateListener(new ServerConnectionStateListener() {
+				@Override
+				public void connectionStateChanged(boolean arg0) {
+					System.out.println("connectionStateChanged=" + arg0);
+
+				}
+			});
 
 			Group group = server.addGroup("group");
 			group.setActive(true);
@@ -49,6 +64,7 @@ public class Test {
 			Map<String, Item> map = new HashMap<String, Item>();
 			map = group.addItems(test);
 
+			// read item
 			for (String s : map.keySet()) {
 				Item it = map.get(s);
 				JIVariant value = it.read(true).getValue();
@@ -56,13 +72,61 @@ public class Test {
 				System.out.println("item:" + it.getId() + ",value:" + s1);
 			}
 
-			// write value
+			// write item
 			item.setActive(true);
 			item.write(new JIVariant("1000"));
 
 			Thread.sleep(1000);
-			System.out.println("write item:" + item.getId() + ",value:"
-					+ item.read(true).getValue().getObject().toString() + " <------==");
+			System.out.println(
+					"write item:" + item.getId() + ",value:" + item.read(true).getValue().getObject().toString());
+
+			// data change
+			group.attach(new IOPCDataCallback() {
+
+				@Override
+				public void writeComplete(int arg0, int arg1, int arg2, ResultSet<Integer> arg3) {
+					// TODO Auto-generated method stub
+
+				}
+
+				@Override
+				public void readComplete(int arg0, int arg1, int arg2, int arg3,
+						KeyedResultSet<Integer, ValueData> arg4) {
+					// TODO Auto-generated method stub
+
+				}
+
+				@Override
+				public void dataChange(int arg0, int arg1, int arg2, int arg3,
+						KeyedResultSet<Integer, ValueData> arg4) {
+					System.out.println(
+							" arg0=" + arg0 + "\r\n arg1=" + arg1 + "\r\n arg2=" + arg2 + "\r\n arg3=" + arg3 + "\r\n");
+
+					for (int i = 0; i < arg4.size(); i++) {
+						Integer key = arg4.get(i).getKey();
+						ValueData value = arg4.get(i).getValue();
+						try {
+							JIVariant value2 = value.getValue();
+							String s = value2.getObject().toString();
+							System.out.println("key=" + key + ",value=" + s);
+						} catch (JIException e) {
+							e.printStackTrace();
+						}
+					}
+
+				}
+
+				@Override
+				public void cancelComplete(int arg0, int arg1) {
+					// TODO Auto-generated method stub
+
+				}
+			});
+
+			while (true) {
+				System.out.println("show changed dates:");
+				Thread.sleep(3000);
+			}
 
 		} catch (final JIException e) {
 			e.printStackTrace();
